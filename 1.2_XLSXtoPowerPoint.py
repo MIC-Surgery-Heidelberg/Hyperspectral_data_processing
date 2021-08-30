e08f17d05e8edf97769399360edb32a6fed58e2d
MIC-Surgery-Heidelberg <67436320+MIC-Surgery-Heidelberg@users.noreply.github.com>
2021-08-30 12:07:49 +0200

    Update 1.2_XLSXtoPowerPoint.py

--- 1.2_XLSXtoPowerPoint.py
+++ 1.2_XLSXtoPowerPoint.py
@@ -1,5 +1,5 @@
 """
-@author: Alexander Studier-Fischer, Jan Odenthal, Berkin Özdemir, University of Heidelberg
+@author: Alexander Studier-Fischer, Jan Odenthal, Berkin Özdemir, Isabella Camplisson University of Heidelberg
 """
 from pptx import *
 import pandas as pd
